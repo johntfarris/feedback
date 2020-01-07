38f64bde5d54fa205b7ae26d27462639b82aa59c
johntfarris <johnfarris217@gmail.com>
2020-01-07 16:21:02 -0600

    fixed prod page

--- config/prod.js
+++ config/prod.js
@@ -6,9 +6,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
-
-//prod id 561237635715-d9cbv4sgdl0col76jphb92dri4elb8f6.apps.googleusercontent.com
-//prod secret vaa74_NWKiE1SBRaYyiUZzEt
-
-// emaily-prod credentials: prod dP984gvmNil3FjpV
-// mongodb+srv://prod:<password>@cluster0-vwyv5.mongodb.net/test?retryWrites=true&w=majority
