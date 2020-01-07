8baa06addd5d6344bf569a706bc02fdd7f1724a7
johntfarris <johnfarris217@gmail.com>
2020-01-07 16:15:38 -0600

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+// prod.js - do commit
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+//prod id 561237635715-d9cbv4sgdl0col76jphb92dri4elb8f6.apps.googleusercontent.com
+//prod secret vaa74_NWKiE1SBRaYyiUZzEt
+
+// emaily-prod credentials: prod dP984gvmNil3FjpV
+// mongodb+srv://prod:<password>@cluster0-vwyv5.mongodb.net/test?retryWrites=true&w=majority
